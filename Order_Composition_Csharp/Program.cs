c5ff431edd73711fc6854148df056872ab5cab06
Davidson Dias <davidsonsd16@gmail.com>
2019-10-27 16:26:25 -0300

    Class Client

--- Order_Composition_Csharp/Program.cs
+++ Order_Composition_Csharp/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Order_Composition_Csharp.Entities;
 
 namespace Order_Composition_Csharp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Client client = new Client("Davidson Dias", "davidsonsd16@gmail", DateTime.Parse("16/09/1995"));
+
+            Console.WriteLine(client);
         }
     }
 }
