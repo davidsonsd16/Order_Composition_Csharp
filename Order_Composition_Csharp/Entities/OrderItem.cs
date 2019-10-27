f9da79a2af5717b24a28a33227dde022a31d6318
Davidson Dias <davidsonsd16@gmail.com>
2019-10-27 16:53:26 -0300

    Class OrderItem and Pruduct

---
+++ Order_Composition_Csharp/Entities/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order_Composition_Csharp.Entities {
+    class OrderItem {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        //public OrderItem() { }
+
+        public OrderItem(Product product, int quantity) {
+            Quantity = quantity;
+            Price = product.Price;
+            Product = product;
+        }
+
+        public double SubTotal() {
+            return Price * Quantity;
+        }
+        public override string ToString() {
+            return Product.Name
+                + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: " + Quantity
+                + ", Subtotal: $" + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Order_Composition_Csharp/Program.cs
+++ Order_Composition_Csharp/Program.cs
@@ -5,9 +5,19 @@ namespace Order_Composition_Csharp {
     class Program {
         static void Main(string[] args) {
 
-            Client client = new Client("Davidson Dias", "davidsonsd16@gmail", DateTime.Parse("16/09/1995"));
+            Client client = new Client("Davidson Dias", "davidsonsd16@gmail.com", DateTime.Parse("16/09/1995"));
 
             Console.WriteLine(client);
+
+            Product product = new Product("TV", 1000.00);
+            OrderItem orderItem = new OrderItem(product, 4);
+
+            Console.WriteLine(orderItem);
+
+            product.Price = 1200.00;
+
+            Console.WriteLine(product);
+            Console.WriteLine(orderItem);
         }
     }
 }
